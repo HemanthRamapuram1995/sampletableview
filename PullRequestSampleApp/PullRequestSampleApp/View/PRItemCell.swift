f4521f8c699f6e68c4c1abd43bf7c964b1891797
HemanthRamapuram1995 <91176532+HemanthRamapuram1995@users.noreply.github.com>
2021-12-17 16:33:51 +0530

    Created PRItem UI
    Created PRItemCell
    Added PRICon Image

---
+++ PullRequestSampleApp/PullRequestSampleApp/View/PRItemCell.swift
@@ -0,0 +1,23 @@
+//
+//  PRItemCell.swift
+//  PullRequestSampleApp
+//
+//  Created by Hemanth Ramapuram on 17/12/21.
+//
+
+import UIKit
+
+class PRItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
